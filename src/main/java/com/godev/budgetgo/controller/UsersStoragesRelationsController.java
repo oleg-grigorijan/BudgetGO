d8c0391c3f95471ddf57df5927455b61430ef771
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-10-23 20:57:27 +0300

    UsersStoragesRelationsController is added

---
+++ src/main/java/com/godev/budgetgo/controller/UsersStoragesRelationsController.java
@@ -0,0 +1,75 @@
+package com.godev.budgetgo.controller;
+
+import com.godev.budgetgo.dto.UserStorageRelationsCreationDto;
+import com.godev.budgetgo.dto.UserStorageRelationsInfoDto;
+import com.godev.budgetgo.dto.UserStorageRelationsPatchDto;
+import com.godev.budgetgo.entity.UserStorageKey;
+import com.godev.budgetgo.service.UsersStoragesRelationsService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/storages/{storageId}/users")
+public class UsersStoragesRelationsController {
+
+    private final UsersStoragesRelationsService relationsService;
+
+    public UsersStoragesRelationsController(UsersStoragesRelationsService relationsService) {
+        this.relationsService = relationsService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserStorageRelationsInfoDto> getAll(@PathVariable Long storageId) {
+        return relationsService.findByStorageId(storageId);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(
+            HttpServletResponse response,
+            @PathVariable Long storageId,
+            @RequestBody UserStorageRelationsCreationDto creationDto
+    ) {
+        relationsService.create(storageId, creationDto);
+        response.addHeader(
+                "Location",
+                "/api/storages/" + storageId
+                        + "/users/" + creationDto.getUserId()
+        );
+    }
+
+    @GetMapping("/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserStorageRelationsInfoDto get(
+            @PathVariable Long storageId,
+            @PathVariable Long userId
+    ) {
+        return relationsService.findById(new UserStorageKey(userId, storageId));
+    }
+
+    @PatchMapping("/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserStorageRelationsInfoDto patch(
+            @PathVariable Long storageId,
+            @PathVariable Long userId,
+            @RequestBody UserStorageRelationsPatchDto patchDto
+    ) {
+        return relationsService.patch(
+                new UserStorageKey(userId, storageId),
+                patchDto
+        );
+    }
+
+    @DeleteMapping("/{userId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(
+            @PathVariable Long storageId,
+            @PathVariable Long userId
+    ) {
+        relationsService.deleteById(new UserStorageKey(userId, storageId));
+    }
+}
