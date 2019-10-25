4900a698637a183e51f38620e1aa87fc4366e5b3
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-10-25 20:44:00 +0300

    CategoriesDataService is added

---
+++ src/main/java/com/godev/budgetgo/exception/CategoryNotFoundException.java
@@ -0,0 +1,4 @@
+package com.godev.budgetgo.exception;
+
+public class CategoryNotFoundException extends NotFoundExcepion {
+}
---
+++ src/main/java/com/godev/budgetgo/service/data/implementations/CategoriesDataServiceImpl.java
@@ -0,0 +1,15 @@
+package com.godev.budgetgo.service.data.implementations;
+
+import com.godev.budgetgo.entity.Category;
+import com.godev.budgetgo.exception.CategoryNotFoundException;
+import com.godev.budgetgo.repository.CategoriesRepository;
+import com.godev.budgetgo.service.data.CategoriesDataService;
+
+class CategoriesDataServiceImpl
+        extends AbstractDataService<Category, Long>
+        implements CategoriesDataService {
+
+    public CategoriesDataServiceImpl(CategoriesRepository repository) {
+        super(repository, CategoryNotFoundException::new);
+    }
+}
