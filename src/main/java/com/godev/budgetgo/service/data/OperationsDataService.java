b75581e34cf79c675fbfe9facf0131ddfede512b
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-10-26 23:27:18 +0300

    OperationsDataService is added

---
+++ src/main/java/com/godev/budgetgo/service/data/OperationsDataService.java
@@ -0,0 +1,23 @@
+package com.godev.budgetgo.service.data;
+
+import com.godev.budgetgo.entity.Operation;
+import com.godev.budgetgo.entity.Storage;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface OperationsDataService extends DataService<Operation, Long> {
+    List<Operation> getByDateBetween(LocalDate from, LocalDate to);
+
+    List<Operation> getByStorageAndDateBetween(Storage storage, LocalDate from, LocalDate to);
+
+    /**
+     * Removes all operations with given storage in data source.
+     * <p>
+     * Method doesn't control storage balance validity
+     * and should be used only while storage removing.
+     *
+     * @param storage storage criteria for removing
+     */
+    void deleteByStorage(Storage storage);
+}
--- src/main/java/com/godev/budgetgo/service/data/implementations/StoragesDataServiceImpl.java
+++ src/main/java/com/godev/budgetgo/service/data/implementations/StoragesDataServiceImpl.java
@@ -3,15 +3,30 @@ package com.godev.budgetgo.service.data.implementations;
 import com.godev.budgetgo.entity.Storage;
 import com.godev.budgetgo.exception.StorageNotFoundException;
 import com.godev.budgetgo.repository.StoragesRepository;
+import com.godev.budgetgo.service.data.OperationsDataService;
 import com.godev.budgetgo.service.data.StoragesDataService;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 class StoragesDataServiceImpl
         extends AbstractDataService<Storage, Long>
         implements StoragesDataService {
 
-    public StoragesDataServiceImpl(StoragesRepository repository) {
+    private final OperationsDataService operationsDataService;
+
+    public StoragesDataServiceImpl(
+            StoragesRepository repository,
+            OperationsDataService operationsDataService) {
         super(repository, StorageNotFoundException::new);
+        this.operationsDataService = operationsDataService;
+    }
+
+    @Transactional
+    @Override
+    public void delete(Storage entity) {
+        operationsDataService.deleteByStorage(entity);
+        super.delete(entity);
     }
 }
