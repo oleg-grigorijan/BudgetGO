9c52fe37f5f985ead9ef26cd800583ffefa4c011
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-10-26 16:10:29 +0300

    Removing Storage with no references in UserStorageRelations is added

--- src/main/java/com/godev/budgetgo/service/data/implementations/UsersStoragesRelationsDataServiceImpl.java
+++ src/main/java/com/godev/budgetgo/service/data/implementations/UsersStoragesRelationsDataServiceImpl.java
@@ -1,12 +1,15 @@
 package com.godev.budgetgo.service.data.implementations;
 
+import com.godev.budgetgo.entity.Storage;
 import com.godev.budgetgo.entity.UserStorageKey;
 import com.godev.budgetgo.entity.UserStorageRelations;
 import com.godev.budgetgo.exception.UserStorageRelationsNotFoundException;
 import com.godev.budgetgo.repository.UsersStoragesRelationsRepository;
+import com.godev.budgetgo.service.data.StoragesDataService;
 import com.godev.budgetgo.service.data.UsersStoragesRelationsDataService;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -15,15 +18,28 @@ class UsersStoragesRelationsDataServiceImpl
         implements UsersStoragesRelationsDataService {
 
     private final UsersStoragesRelationsRepository repository;
+    private final StoragesDataService storagesDataService;
 
     public UsersStoragesRelationsDataServiceImpl(
-            UsersStoragesRelationsRepository repository) {
+            UsersStoragesRelationsRepository repository,
+            StoragesDataService storagesDataService) {
         super(repository, UserStorageRelationsNotFoundException::new);
         this.repository = repository;
+        this.storagesDataService = storagesDataService;
     }
 
     @Override
     public List<UserStorageRelations> getByStorageId(Long storageId) {
         return repository.findByStorageId(storageId);
+    }
+
+    @Transactional
+    @Override
+    public void delete(UserStorageRelations entity) {
+        super.delete(entity);
+        Storage storage = entity.getStorage();
+        if (getByStorageId(storage.getId()).isEmpty()) {
+            storagesDataService.delete(storage);
+        }
     }
 }
