0697455b07421ccf9452843482dda25843d77a2d
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-10-27 11:29:07 +0300

    CurrenciesRequestService extends RequestService

--- src/main/java/com/godev/budgetgo/service/request/implementations/CurrenciesRequestServiceImpl.java
+++ src/main/java/com/godev/budgetgo/service/request/implementations/CurrenciesRequestServiceImpl.java
@@ -1,39 +1,26 @@
 package com.godev.budgetgo.service.request.implementations;
 
+import com.godev.budgetgo.dto.CurrencyCreationDto;
 import com.godev.budgetgo.dto.CurrencyInfoDto;
+import com.godev.budgetgo.dto.CurrencyPatchesDto;
+import com.godev.budgetgo.entity.Currency;
 import com.godev.budgetgo.service.data.CurrenciesDataService;
+import com.godev.budgetgo.service.factory.CurrenciesFactory;
 import com.godev.budgetgo.service.factory.CurrencyDtoFactory;
+import com.godev.budgetgo.service.merger.CurrenciesMerger;
 import com.godev.budgetgo.service.request.CurrenciesRequestService;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
-class CurrenciesRequestServiceImpl implements CurrenciesRequestService {
-
-    private final CurrenciesDataService dataService;
-    private final CurrencyDtoFactory dtoFactory;
+class CurrenciesRequestServiceImpl
+        extends AbstractRequestService<Currency, Long, CurrencyInfoDto, CurrencyCreationDto, CurrencyPatchesDto>
+        implements CurrenciesRequestService {
 
     public CurrenciesRequestServiceImpl(
             CurrenciesDataService dataService,
-            CurrencyDtoFactory dtoFactory
-    ) {
-        this.dataService = dataService;
-        this.dtoFactory = dtoFactory;
-    }
-
-    @Override
-    public CurrencyInfoDto getById(Long id) {
-        return dtoFactory.createFrom(dataService.getById(id));
-    }
-
-    @Override
-    public List<CurrencyInfoDto> getAll() {
-        return dataService
-                .getAll()
-                .stream()
-                .map(dtoFactory::createFrom)
-                .collect(Collectors.toList());
+            CurrenciesFactory entitiesFactory,
+            CurrencyDtoFactory dtoFactory,
+            CurrenciesMerger merger) {
+        super(dataService, entitiesFactory, dtoFactory, merger);
     }
 }
