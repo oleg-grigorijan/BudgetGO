016e284c2318e18a242481867101a22469066f2a
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-10-27 11:37:01 +0300

    CurrenciesController POST and PATCH request mapping is added

--- src/main/java/com/godev/budgetgo/controller/CurrenciesController.java
+++ src/main/java/com/godev/budgetgo/controller/CurrenciesController.java
@@ -1,10 +1,13 @@
 package com.godev.budgetgo.controller;
 
+import com.godev.budgetgo.dto.CurrencyCreationDto;
 import com.godev.budgetgo.dto.CurrencyInfoDto;
+import com.godev.budgetgo.dto.CurrencyPatchesDto;
 import com.godev.budgetgo.service.request.CurrenciesRequestService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
@@ -23,9 +26,22 @@ public class CurrenciesController {
         return currenciesService.getAll();
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(HttpServletResponse response, @RequestBody CurrencyCreationDto creationDto) {
+        Long newOperationId = currenciesService.create(creationDto).getId();
+        response.addHeader("Location", "/api/currencies/" + newOperationId);
+    }
+
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public CurrencyInfoDto get(@PathVariable Long id) {
         return currenciesService.getById(id);
+    }
+
+    @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CurrencyInfoDto patch(@PathVariable Long id, @RequestBody CurrencyPatchesDto patches) {
+        return currenciesService.patch(id, patches);
     }
 }
