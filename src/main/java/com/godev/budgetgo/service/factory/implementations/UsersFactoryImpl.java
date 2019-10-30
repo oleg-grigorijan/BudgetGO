ad4e9f83cfc0b72bf6301d60783cf27ecca536ae
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-10-30 20:24:13 +0300

    Password hashing is added

--- src/main/java/com/godev/budgetgo/service/factory/implementations/UsersFactoryImpl.java
+++ src/main/java/com/godev/budgetgo/service/factory/implementations/UsersFactoryImpl.java
@@ -3,10 +3,17 @@ package com.godev.budgetgo.service.factory.implementations;
 import com.godev.budgetgo.dto.UserCreationDto;
 import com.godev.budgetgo.entity.User;
 import com.godev.budgetgo.service.factory.UsersFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 class UsersFactoryImpl implements UsersFactory {
+    private final PasswordEncoder passwordEncoder;
+
+    public UsersFactoryImpl(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     public User createFrom(UserCreationDto dto) {
         User user = new User();
@@ -14,7 +21,7 @@ class UsersFactoryImpl implements UsersFactory {
         user.setEmail(dto.getEmail());
         user.setName(dto.getName());
         user.setSurname(dto.getSurname());
-        user.setPasswordHash(dto.getPassword()); // TODO: Hashing
+        user.setPasswordHash(passwordEncoder.encode(dto.getPassword()));
         user.setEmailPublic(true);
         return user;
     }
