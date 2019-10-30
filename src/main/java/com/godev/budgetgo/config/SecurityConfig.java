3f37a1012b75fa699219982c528f6d061953d00f
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-10-30 20:25:26 +0300

    Sessions and csrf are disabled

--- src/main/java/com/godev/budgetgo/config/SecurityConfig.java
+++ src/main/java/com/godev/budgetgo/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -15,6 +16,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.csrf().disable();
         http.authorizeRequests()
                 .antMatchers("/**")
                 .permitAll();
