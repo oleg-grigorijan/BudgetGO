c990274adbfd449327a69a5e93830026282b3c16
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-10-31 11:27:10 +0300

    Creating UserStorageRelations while Storage creation

--- src/main/java/com/godev/budgetgo/service/data/implementations/StoragesDataServiceImpl.java
+++ src/main/java/com/godev/budgetgo/service/data/implementations/StoragesDataServiceImpl.java
@@ -1,10 +1,12 @@
 package com.godev.budgetgo.service.data.implementations;
 
-import com.godev.budgetgo.entity.Storage;
+import com.godev.budgetgo.entity.*;
 import com.godev.budgetgo.exception.StorageNotFoundException;
 import com.godev.budgetgo.repository.StoragesRepository;
 import com.godev.budgetgo.service.data.OperationsDataService;
 import com.godev.budgetgo.service.data.StoragesDataService;
+import com.godev.budgetgo.service.data.UsersStoragesRelationsDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -15,12 +17,34 @@ class StoragesDataServiceImpl
         implements StoragesDataService {
 
     private final OperationsDataService operationsDataService;
+    private UsersStoragesRelationsDataService relationsDataService;
 
     public StoragesDataServiceImpl(
             StoragesRepository repository,
             OperationsDataService operationsDataService) {
         super(repository, StorageNotFoundException::new);
         this.operationsDataService = operationsDataService;
+    }
+
+    @Autowired
+    public void setRelationsDataService(UsersStoragesRelationsDataService relationsDataService) {
+        this.relationsDataService = relationsDataService;
+    }
+
+    @Transactional
+    @Override
+    public Storage addWithCreator(Storage entity, User creator) {
+        Storage storage = super.add(entity);
+
+        UserStorageRelations relations = new UserStorageRelations();
+        relations.setId(new UserStorageKey(creator.getId(), storage.getId()));
+        relations.setUser(creator);
+        relations.setStorage(storage);
+        relations.setUserRole(UserStorageRole.CREATOR);
+        relations.setIncludedInUserStatistics(true);
+        relationsDataService.add(relations);
+
+        return storage;
     }
 
     @Transactional
--- src/main/java/com/godev/budgetgo/service/request/implementations/StoragesRequestServiceImpl.java
+++ src/main/java/com/godev/budgetgo/service/request/implementations/StoragesRequestServiceImpl.java
@@ -1,9 +1,11 @@
 package com.godev.budgetgo.service.request.implementations;
 
+import com.godev.budgetgo.auth.AuthenticationFacade;
 import com.godev.budgetgo.dto.StorageCreationDto;
 import com.godev.budgetgo.dto.StorageInfoDto;
 import com.godev.budgetgo.dto.StoragePatchesDto;
 import com.godev.budgetgo.entity.Storage;
+import com.godev.budgetgo.entity.User;
 import com.godev.budgetgo.service.data.StoragesDataService;
 import com.godev.budgetgo.service.factory.StorageDtoFactory;
 import com.godev.budgetgo.service.factory.StoragesFactory;
@@ -16,12 +18,31 @@ class StoragesRequestServiceImpl
         extends AbstractRequestService<Storage, Long, StorageInfoDto, StorageCreationDto, StoragePatchesDto>
         implements StoragesRequestService {
 
+    private final StoragesDataService dataService;
+    private final StoragesFactory entitiesFactory;
+    private final StorageDtoFactory dtoFactory;
+    private final AuthenticationFacade authenticationFacade;
+
     public StoragesRequestServiceImpl(
             StoragesDataService dataService,
             StoragesFactory entitiesFactory,
             StorageDtoFactory dtoFactory,
-            StoragesMerger merger
+            StoragesMerger merger,
+            AuthenticationFacade authenticationFacade
     ) {
         super(dataService, entitiesFactory, dtoFactory, merger);
+        this.dataService = dataService;
+        this.entitiesFactory = entitiesFactory;
+        this.dtoFactory = dtoFactory;
+        this.authenticationFacade = authenticationFacade;
+    }
+
+    @Override
+    public StorageInfoDto create(StorageCreationDto creationDto) {
+        // TODO: Validation
+        Storage entity = entitiesFactory.createFrom(creationDto);
+        User creator = authenticationFacade.getAuthenticatedUser();
+        dataService.addWithCreator(entity, creator);
+        return dtoFactory.createFrom(entity);
     }
 }
