fe1d29118daa08f8ea033e7e1a165e564ecf506b
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-11-01 12:01:17 +0300

    UsersAuthorizationService is implemented

--- src/main/java/com/godev/budgetgo/service/authorization/implementation/UsersAuthorizationServiceImpl.java
+++ src/main/java/com/godev/budgetgo/service/authorization/implementation/UsersAuthorizationServiceImpl.java
@@ -1,32 +1,41 @@
 package com.godev.budgetgo.service.authorization.implementation;
 
+import com.godev.budgetgo.auth.AuthenticationFacade;
 import com.godev.budgetgo.dto.UserCreationDto;
 import com.godev.budgetgo.dto.UserPatchesDto;
 import com.godev.budgetgo.entity.User;
+import com.godev.budgetgo.exception.UserAccessDeniedException;
 import com.godev.budgetgo.service.authorization.UsersAuthorizationService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 class UsersAuthorizationServiceImpl implements UsersAuthorizationService {
+    private final AuthenticationFacade authenticationFacade;
+
+    public UsersAuthorizationServiceImpl(AuthenticationFacade authenticationFacade) {
+        this.authenticationFacade = authenticationFacade;
+    }
+
     @Override
     public List<User> getAllAuthorizedEntities() {
-        return null;
+        return new ArrayList<>();
     }
 
     @Override
     public void authorizeGet(User entity) {
-
     }
 
     @Override
     public void authorizeCreate(UserCreationDto creationDto) {
-
     }
 
     @Override
     public void authorizePatch(User entity, UserPatchesDto patchesDto) {
-
+        if (!entity.getLogin().equals(authenticationFacade.getAuthentication().getName())) {
+            throw new UserAccessDeniedException();
+        }
     }
 }
