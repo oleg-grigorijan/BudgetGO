08e48ebf777bf3b6928ca66da93e91b3579f64b7
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-11-07 22:49:34 +0300

    CollectionsRepository is added

---
+++ src/main/java/com/godev/budgetgo/repository/CollectionsRepository.java
@@ -0,0 +1,11 @@
+package com.godev.budgetgo.repository;
+
+import com.godev.budgetgo.entity.Collection;
+import com.godev.budgetgo.entity.User;
+import com.godev.budgetgo.entity.UserCategoryKey;
+
+import java.util.List;
+
+public interface CollectionsRepository extends Repository<Collection, UserCategoryKey> {
+    List<Collection> findByUser(User user);
+}
