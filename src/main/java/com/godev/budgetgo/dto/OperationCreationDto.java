afc0d5c2461fe396f3a8b6f5dce6c0a0ff62c3c2
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-11-09 20:31:57 +0300

    Operations validation is added

--- src/main/java/com/godev/budgetgo/dto/OperationCreationDto.java
+++ src/main/java/com/godev/budgetgo/dto/OperationCreationDto.java
@@ -1,16 +1,29 @@
 package com.godev.budgetgo.dto;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.godev.budgetgo.entity.Operation;
 import com.godev.budgetgo.json.LocalDateDeserializer;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 public class OperationCreationDto {
+
+    @NotNull
     private Long storageId;
+
     private long moneyDelta;
+
+    @NotNull
     private Long categoryId;
+
+    @PastOrPresent
     @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate date = LocalDate.now();
+
+    @Size(max = Operation.DESCRIPTION_MAX_LENGTH)
     private String description = "";
 
     public Long getStorageId() {
--- src/main/java/com/godev/budgetgo/dto/OperationPatchesDto.java
+++ src/main/java/com/godev/budgetgo/dto/OperationPatchesDto.java
@@ -1,15 +1,24 @@
 package com.godev.budgetgo.dto;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.godev.budgetgo.entity.Operation;
 import com.godev.budgetgo.json.LocalDateDeserializer;
 
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 public class OperationPatchesDto {
+
     private Long moneyDelta;
+
     private Long categoryId;
+
+    @PastOrPresent
     @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate date;
+
+    @Size(max = Operation.DESCRIPTION_MAX_LENGTH)
     private String description;
 
     public Long getMoneyDelta() {
