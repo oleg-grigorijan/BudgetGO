c509dbac48c82c7ff4c4286a77a7db59ae94aa4e
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-11-09 21:09:29 +0300

    Collections validation is added

--- src/main/java/com/godev/budgetgo/dto/CollectionCreationDto.java
+++ src/main/java/com/godev/budgetgo/dto/CollectionCreationDto.java
@@ -1,6 +1,10 @@
 package com.godev.budgetgo.dto;
 
+import javax.validation.constraints.NotNull;
+
 public class CollectionCreationDto {
+
+    @NotNull
     private Long categoryId;
 
     public Long getCategoryId() {
