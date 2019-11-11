d6c2100b6d0eb421ae0a503db25e52de2f7bca17
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-11-11 17:21:53 +0300

    OperationsKeySequence entity is added

---
+++ src/main/java/com/godev/budgetgo/entity/OperationsKeySequence.java
@@ -0,0 +1,33 @@
+package com.godev.budgetgo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "operations_key_sequence")
+public class OperationsKeySequence {
+    @Id
+    @Column(name = "storage_id")
+    private Long storageId;
+
+    @Column(name = "next_operation_id")
+    private Long nextOperationId;
+
+    public Long getStorageId() {
+        return storageId;
+    }
+
+    public void setStorageId(Long storageId) {
+        this.storageId = storageId;
+    }
+
+    public Long getNextOperationId() {
+        return nextOperationId;
+    }
+
+    public void setNextOperationId(Long nextOperationId) {
+        this.nextOperationId = nextOperationId;
+    }
+}
