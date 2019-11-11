cfe2b2c18567bedf21a736c4a3888cb893dbaa57
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-11-11 17:22:20 +0300

    OperationsKeySequenceRepository is added

---
+++ src/main/java/com/godev/budgetgo/repository/OperationsKeySequenceRepository.java
@@ -0,0 +1,6 @@
+package com.godev.budgetgo.repository;
+
+import com.godev.budgetgo.entity.OperationsKeySequence;
+
+public interface OperationsKeySequenceRepository extends Repository<OperationsKeySequence, Long> {
+}
---
+++ src/main/java/com/godev/budgetgo/repository/implementations/OperationsKeySequenceRepositoryImpl.java
@@ -0,0 +1,15 @@
+package com.godev.budgetgo.repository.implementations;
+
+import com.godev.budgetgo.entity.OperationsKeySequence;
+import com.godev.budgetgo.repository.OperationsKeySequenceRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+class OperationsKeySequenceRepositoryImpl
+        extends AbstractRepository<OperationsKeySequence, Long>
+        implements OperationsKeySequenceRepository {
+
+    OperationsKeySequenceRepositoryImpl() {
+        super(OperationsKeySequence.class);
+    }
+}
