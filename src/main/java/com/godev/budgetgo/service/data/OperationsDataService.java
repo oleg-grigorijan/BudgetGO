2c2ea63b21111683050a51da91056c2db1fd8a73
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-11-11 23:43:22 +0300

    Restoring storage initial balance while removing operations by storage

--- src/main/java/com/godev/budgetgo/service/data/OperationsDataService.java
+++ src/main/java/com/godev/budgetgo/service/data/OperationsDataService.java
@@ -12,13 +12,5 @@ public interface OperationsDataService extends DataService<Operation, StorageOpe
 
     List<Operation> getByStorageAndDateBetween(Storage storage, LocalDate from, LocalDate to);
 
-    /**
-     * Removes all operations with given storage in data source.
-     * <p>
-     * Method doesn't control storage balance validity
-     * and should be used only while storage removing.
-     *
-     * @param storage storage criteria for removing
-     */
     void deleteByStorage(Storage storage);
 }
