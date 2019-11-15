babef3b2f2d758a6fbd78d16b59ec4a6818eca11
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-11-15 10:35:30 +0300

    BadRequestAdvice handles more exceptions and is made restful

--- src/main/java/com/godev/budgetgo/advice/BadRequestAdvice.java
+++ src/main/java/com/godev/budgetgo/advice/BadRequestAdvice.java
@@ -1,16 +1,51 @@
 package com.godev.budgetgo.advice;
 
+import com.godev.budgetgo.dto.ErrorInfoDto;
 import com.godev.budgetgo.exception.BadRequestException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestAdvice {
 
     @ExceptionHandler(BadRequestException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    void handle() {
+    ErrorInfoDto handle(BadRequestException ex) {
+        return new ErrorInfoDto(
+                HttpStatus.BAD_REQUEST,
+                ex.getMessage()
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ErrorInfoDto handle(MethodArgumentNotValidException ex) {
+        List<String> errors = new ArrayList<>();
+        ex.getBindingResult().getFieldErrors().forEach(
+                error -> errors.add(error.getField() + ": " + error.getDefaultMessage())
+        );
+        return new ErrorInfoDto(
+                HttpStatus.BAD_REQUEST,
+                "Validation error",
+                String.join("; ", errors)
+        );
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    ErrorInfoDto handle(HttpServletRequest request, HttpMessageNotReadableException ex) {
+        return new ErrorInfoDto(
+                HttpStatus.BAD_REQUEST,
+                "Request parsing error"
+        );
     }
 }
