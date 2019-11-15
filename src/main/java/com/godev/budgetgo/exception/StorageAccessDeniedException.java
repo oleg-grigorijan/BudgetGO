ce20a69db1d95e5ed09df7713f7a96ce70f7ee25
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-11-15 10:34:05 +0300

    ForbiddenAdvice is made restful

--- src/main/java/com/godev/budgetgo/exception/StorageAccessDeniedException.java
+++ src/main/java/com/godev/budgetgo/exception/StorageAccessDeniedException.java
@@ -1,4 +1,18 @@
 package com.godev.budgetgo.exception;
 
+import org.springframework.security.access.AccessDeniedException;
+
 public class StorageAccessDeniedException extends AccessDeniedException {
+
+    public StorageAccessDeniedException() {
+        super("Storage access denied");
+    }
+
+    public StorageAccessDeniedException(String message) {
+        super(message);
+    }
+
+    public StorageAccessDeniedException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
