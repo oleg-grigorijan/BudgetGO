de3dd5ebac84013ae7485fc681792cb76aee7a7a
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-11-20 18:06:17 +0300

    OperationsConverter is added

--- src/main/java/com/godev/budgetgo/service/factory/ConverterFactory.java
+++
@@ -1,9 +0,0 @@
-package com.godev.budgetgo.service.factory;
-
-/**
- * @param <T> from
- * @param <V> to
- */
-public interface ConverterFactory<T, V> {
-    V createFrom(T o);
-}
--- src/main/java/com/godev/budgetgo/service/merger/OperationsMerger.java
+++
@@ -1,7 +0,0 @@
-package com.godev.budgetgo.service.merger;
-
-import com.godev.budgetgo.dto.OperationPatchesDto;
-import com.godev.budgetgo.entity.Operation;
-
-public interface OperationsMerger extends Merger<OperationPatchesDto, Operation> {
-}
