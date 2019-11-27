5e0ac8e3b09c45b349227df1bea2895f3984fa3d
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-11-27 23:19:39 +0300

    MissingServletRequestParameterException handling is added

--- src/main/java/com/godev/budgetgo/advice/BadRequestExceptionsHandler.java
+++ src/main/java/com/godev/budgetgo/advice/BadRequestExceptionsHandler.java
@@ -8,6 +8,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -23,6 +24,11 @@ public class BadRequestExceptionsHandler {
 
     @ExceptionHandler(BadRequestException.class)
     public ErrorInfoDto handle(BadRequestException ex) {
+        return new ErrorInfoDto(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ErrorInfoDto handle(MissingServletRequestParameterException ex) {
         return new ErrorInfoDto(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
 
