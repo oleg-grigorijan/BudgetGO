2bbc2583044e0dcc0c6c1fbafcdf3c0684882ccf
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-12-09 10:45:41 +0300

    UserCategoryDtos and UserCategoriesConverter are updated according to UserCategory entity

--- src/main/java/com/godev/budgetgo/converter/UserCategoriesConverter.java
+++ src/main/java/com/godev/budgetgo/converter/UserCategoriesConverter.java
@@ -2,7 +2,9 @@ package com.godev.budgetgo.converter;
 
 import com.godev.budgetgo.dto.UserCategoryCreationDto;
 import com.godev.budgetgo.dto.UserCategoryInfoDto;
+import com.godev.budgetgo.dto.UserCategoryPatchesDto;
 import com.godev.budgetgo.entity.UserCategory;
 
-public interface UserCategoriesConverter extends EntityConverter<UserCategory, UserCategoryInfoDto>, DtoConverter<UserCategoryCreationDto, UserCategory> {
+public interface UserCategoriesConverter extends EntityConverter<UserCategory, UserCategoryInfoDto>,
+        DtoConverter<UserCategoryCreationDto, UserCategory>, Merger<UserCategory, UserCategoryPatchesDto> {
 }
--- src/main/java/com/godev/budgetgo/dto/UserCategoryInfoDto.java
+++ src/main/java/com/godev/budgetgo/dto/UserCategoryInfoDto.java
@@ -7,11 +7,33 @@ public class UserCategoryInfoDto {
     @JsonProperty("category")
     private CategoryInfoDto categoryInfoDto;
 
+    @JsonProperty("isUsedForIncomes")
+    private boolean usedForIncomes;
+
+    @JsonProperty("isUsedForOutcomes")
+    private boolean usedForOutcomes;
+
     public CategoryInfoDto getCategoryInfoDto() {
         return categoryInfoDto;
     }
 
     public void setCategoryInfoDto(CategoryInfoDto categoryInfoDto) {
         this.categoryInfoDto = categoryInfoDto;
+    }
+
+    public boolean getUsedForIncomes() {
+        return usedForIncomes;
+    }
+
+    public void setUsedForIncomes(boolean usedForIncomes) {
+        this.usedForIncomes = usedForIncomes;
+    }
+
+    public boolean getUsedForOutcomes() {
+        return usedForOutcomes;
+    }
+
+    public void setUsedForOutcomes(boolean usedForOutcomes) {
+        this.usedForOutcomes = usedForOutcomes;
     }
 }
