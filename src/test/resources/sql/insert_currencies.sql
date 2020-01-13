616495209b812bf8feeb7f0b263d98694da85ae8
oleg-grigorijan <oleg.grigorijan@gmail.com>
2020-01-13 23:56:49 +0300

    UsersControllerTest is added

---
+++ src/test/resources/sql/insert_currencies.sql
@@ -0,0 +1,4 @@
+INSERT INTO BUDGETGO_TEST.PUBLIC.CURRENCIES (id, iso_code, name)
+VALUES (1, 'USD', 'United States Dollar');
+INSERT INTO BUDGETGO_TEST.PUBLIC.CURRENCIES (id, iso_code, name)
+VALUES (2, 'EUR', 'Euro');
